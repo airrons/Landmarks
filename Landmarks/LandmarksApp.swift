9f39b4d3ac75dfd7a908ba4c7a8bd7aa47752d2a
airrons <airron@yeah.net>
2021-11-27 22:02:16 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by violet on 2021/11/27.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
